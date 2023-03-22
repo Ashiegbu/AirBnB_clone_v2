2f8260362711a1f47f3129ae1337df2326002c82
madokscharles <charlesmadoks@gmail.com>
2023-03-22 15:30:18 -0700

    Debug repository

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,7 @@
 -- Prepares a MySQL Test server for the project, assign hbnb_test
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
